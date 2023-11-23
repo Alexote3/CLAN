1e725190c7b4080a915b52c1e298d4878536305c
Alexote3 <Axelvaras567@gmail.com>
2023-11-23 20:12:26 -0300

    v1.0.0.7

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello</h1>
+      
+    <h1>La Historia de Axel en la Escuela Nacional de Ranchos</h1>
+
+    <img src="axel.jpg" alt="Axel en la escuela" width="300" height="200"/>
+    
+    <h2>Capítulo 1: El Comienzo</h2>
+    <p>Axel, un joven apasionado por el conocimiento, comenzó su travesía en la Escuela Nacional de Ranchos en Buenos Aires con grandes expectativas. Desde el primer día, se sumergió en el vibrante ambiente educativo de la escuela.</p>
+
+    <h2>Capítulo 2: Descubriendo su Pasión</h2>
+    <p>En el transcurso de los primeros meses, Axel exploró diversas materias y actividades extracurriculares. Fue en una clase de ciencias que descubrió su pasión por la biología, inspirado por un apasionado profesor que despertó su curiosidad por el mundo natural.</p>
+
+    <h2>Capítulo 3: Desafíos y Superación</h2>
+    <p>No todo fue fácil para Axel. Enfrentó desafíos académicos y personales, pero con determinación y el apoyo de sus amigos y profesores, superó cada obstáculo. Aprendió a valorar la importancia de la perseverancia y el trabajo duro.</p>
+
+    <h2>Capítulo 4: Proyectos y Logros</h2>
+    <p>A lo largo de los años, Axel participó en diversos proyectos y competiciones, destacándose por su creatividad y dedicación. Su participación en el equipo de ciencias lo llevó a ganar premios y reconocimientos, haciendo sentir orgullosa a la Escuela Nacional de Ranchos.</p>
+
+    <h2>Capítulo 5: El Graduado</h2>
+    <p>Finalmente, llegó el día de la graduación. Axel, con su diploma en mano, reflexionó sobre su tiempo en la escuela y agradeció a todos quienes lo apoyaron. Listo para enfrentar nuevos desafíos, dejó la Escuela Nacional de Ranchos con recuerdos inolvidables y un futuro lleno de posibilidades.</p>
+
+    <h2>Epílogo: Un Nuevo Comienzo</h2>
+    <p>La historia de Axel en la Escuela Nacional de Ranchos fue solo el principio. Ahora, con una base sólida y un espíritu lleno de determinación, se embarca en nuevas aventuras, listo para contribuir al mundo con su conocimiento y pasión.</p>
+
+    <p>¡Y así concluye la inspiradora historia de Axel!</p>
       </header>
     </div>
   );
